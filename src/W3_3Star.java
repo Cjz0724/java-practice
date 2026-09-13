2e5a06e926019bfdb4a69fbb4fe0c9a226d23df3
Cjz0724 <3037623778@qq.com>
2026-09-13 15:51:36 +0800

    practice

--- src/W3_3Star.java
+++ src/W3_3Star.java
@@ -43,10 +43,10 @@ public class W3_3Star {
         System.out.println();
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i-1; j++) {
+            for (int j = 1; j <= i - 1; j++) {
                 System.out.print(" ");
             }
-            for (int k = 1; k <= 2*(n-(i-1))-1; k++) {
+            for (int k = 1; k <= 2 * (n - (i - 1)) - 1; k++) {
                 System.out.print("*");
             }
             System.out.println();
