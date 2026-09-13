934ac1e6ae49f5a64d807695ef9a5035aecf01ac
Cjz0724 <3037623778@qq.com>
2026-09-14 00:49:04 +0800

    practice

--- src/W3_5ArrayInput.java
+++ src/W3_5ArrayInput.java
@@ -6,16 +6,16 @@ public class W3_5ArrayInput {
         System.out.print("要输入几个数？：");
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for(int i=0;i<n;i++){
-             arr[i] = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-        for(int j=0;j<arr.length;j++){
-            System.out.print(arr[j]+" ");
+        for (int j = 0; j < arr.length; j++) {
+            System.out.print(arr[j] + " ");
         }
         System.out.println();
 
-        for(int k = n-1; k >= 0; k--){
-            System.out.print(arr[k]+" ");
+        for (int k = n - 1; k >= 0; k--) {
+            System.out.print(arr[k] + " ");
 
         }
 
