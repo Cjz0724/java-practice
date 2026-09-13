d31ebbc0cd1b12e8238641b62842df1701920399
Cjz0724 <3037623778@qq.com>
2026-09-13 17:32:25 +0800

    practice

--- src/W3_5ArrayInput.java
+++ src/W3_5ArrayInput.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 【第 3 周】W3_5ArrayInput — 让用户输入数组
  *
@@ -20,8 +22,22 @@
  */
 public class W3_5ArrayInput {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("要输入几个数？：");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+             arr[i] = sc.nextInt();
+        }
+        for(int j=0;j<arr.length;j++){
+            System.out.print(arr[j]+" ");
+        }
+        System.out.println();
 
-        // 在这里写你的代码
+        for(int k = n-1; k >= 0; k--){
+            System.out.print(arr[k]+" ");
+
+        }
 
     }
 }
